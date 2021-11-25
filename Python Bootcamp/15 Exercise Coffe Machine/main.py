50457d05feb79400c3aafb9a5806367205cb0978
carlosp2001 <85173449+carlosp2001@users.noreply.github.com>
2021-11-25 08:04:38 -0600

    Quiz Game True or False

--- Python Bootcamp/15 Exercise Coffe Machine/main.py
+++ Python Bootcamp/15 Exercise Coffe Machine/main.py
@@ -25,31 +25,22 @@ MENU = {
 }
 
 money = 0
-
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-coins = {
-    "quarters": 0.25,
-    "dimes": 0.10,
-    "nickles": 0.05,
-    "pennies": 0.01,
-}
-
 
 # TODO: 7 Make Coffee
 
-def update_resource(usercooffechoice):
+def update_resource(user_coffee_choice):
     global resources
-    for ingredient in MENU[usercooffechoice]['ingredients']:
-        resources[ingredient] -= MENU[usercooffechoice]['ingredients'][ingredient]
+    for ingredient in MENU[user_coffee_choice]['ingredients']:
+        resources[ingredient] -= MENU[user_coffee_choice]['ingredients'][ingredient]
 
 
 # TODO: 3 Print report of all coffee machine resources
-
 
 def print_report():
     print(f"Water: {resources['water']}")
@@ -77,7 +68,7 @@ def process_coins():
     dimes = float(input('how many dimes '))
     nickles = float(input('how many nickles '))
     pennies = float(input('how many pennies '))
-    total = (quarters*coins['quarters'])+(dimes*coins['dimes'])+(nickles*coins['dimes'])+(pennies*coins['pennies'])
+    total = (quarters*0.25)+(dimes*0.1)+(nickles*0.05)+(pennies*0.01)
     return round(total, 2)
 
 
@@ -87,7 +78,6 @@ machine_on = True
 
 while machine_on:
     users_choice = input('What would you like? (espresso/latte/cappuccino)')
-
     # TODO: 2 Turn off the Coffee Machine by entering "off" to the prompt
     if users_choice == 'off':
         machine_on = False
@@ -97,10 +87,10 @@ while machine_on:
         sufficient_resource = check_resources(users_choice)
         if sufficient_resource:
             total_money = process_coins()
-        #TODO: 6 Check transaction successful
+        # TODO: 6 Check transaction successful
             if total_money >= MENU[users_choice]["cost"]:
                 update_resource(users_choice)
-                change=0
+                change = 0
                 if total_money > MENU[users_choice]["cost"]:
                     change = round((total_money-MENU[users_choice]["cost"]), 2)
                     print(f'Here is {change} dollars in change')
@@ -108,5 +98,3 @@ while machine_on:
                 print(f'Here is your {users_choice}. Enjoy')
             else:
                 print("Sorry that's not enough money. Money refunded.")
-
-
